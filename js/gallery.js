fc778805eda84a26211981ace9771f7ad76f03c5
neatexq <Derevyankomisha20122012@gmail.com>
2025-08-12 12:44:59 +0300

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,18 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const gallery = document.getElementById("gallery");
-
-    // Массив с артом
-    const images = [
-        "images/art1.jpg",
-        "images/art2.jpg",
-        // Просто добавляй новые строки сюда
-    ];
-
-    images.forEach(src => {
-        const img = document.createElement("img");
-        img.src = src;
-        img.alt = "Арт";
-        img.loading = "lazy"; // Ленивая загрузка
-        gallery.appendChild(img);
-    });
-});
+[
+    "images/art1.jpg",
+    "images/art2.jpg"
+]
